d42d3a3b0697dfa381a03c3ca0bc718c97cdb915
ca11ba <amirgizatullin176@gmail.com>
2025-05-06 21:07:22 +0300

    Update trips.js

--- app/public/user/js/trips.js
+++ app/public/user/js/trips.js
@@ -49,3 +49,81 @@ document.addEventListener('DOMContentLoaded', () => {
         filterStep2Modal.classList.add('hidden');
     });
 });
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    loadTrips();
+});
+
+async function loadTrips() {
+    try {
+        const response = await fetch('/user/account/trips');
+        if (!response.ok) throw new Error('Failed to fetch');
+
+        const trips = await response.json();
+        const tbody = document.querySelector('.trips-table tbody');
+        if (!tbody) throw new Error('Table body not found');
+
+        tbody.innerHTML = '';
+
+        for (const trip of trips) {
+            const dateObj = new Date(trip.date);
+            const date = dateObj.toLocaleDateString();
+            const time = dateObj.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+
+            const row = document.createElement('tr');
+            row.innerHTML = `
+                <td>${date}</td>
+                <td>${time}</td>
+                <td>Bus</td>
+                <td>70₽</td>
+            `;
+            tbody.appendChild(row);
+        }
+
+
+        const countRow = document.createElement('tr');
+        countRow.innerHTML = `<td colspan="4">Count: ${trips.length}</td>`;
+        tbody.appendChild(countRow);
+
+    } catch (err) {
+        console.error('Error loading trips:', err);
+    }
+}
+
+window.addEventListener('DOMContentLoaded', async () => {
+    try {
+        const response = await fetch('/user/account-data');
+        const user = await response.json();
+
+        if (!response.ok) {
+            console.error(user.error || 'Ошибка получения данных');
+            return;
+        }
+
+
+
+        const sidebarName = document.querySelector('.username');
+        sidebarName.textContent = `${user.last_name} ${user.first_name[0]}.`;
+    } catch (err) {
+        console.error('Ошибка загрузки данных аккаунта:', err);
+    }
+});
+
+
+
+
+
+document.addEventListener('DOMContentLoaded', function() {
+
+    const usernameElement = document.getElementById('user-profile');
+
+    if (usernameElement) {
+        usernameElement.addEventListener('click', function() {
+            window.location.href = 'http://localhost:3000/user/account';
+        });
+    }
+});
+
+
+
