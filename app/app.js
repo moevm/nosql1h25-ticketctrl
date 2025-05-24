4ddec2ea80ae48f55e352a69ba12040997c5bf41
ca11ba <amirgizatullin176@gmail.com>
2025-05-24 18:27:38 +0300

    feat: add controller routes

--- app/app.js
+++ app/app.js
@@ -30,11 +30,24 @@ const accountRoutes = require('./routes/account_user_routes');
 const tripsRoutes = require('./routes/account_trips_routes');
 const finesRoutes = require('./routes/unpaid_routes');
 
+
+
+const authControllerRoutes = require('./routes/auth_controller_routes');
+const controllerAccount = require('./routes/account_controller_routes');
+const controllerPaidFines = require('./routes/acc-controller_paid-fines_routes')
+const controllerUnpaidFines = require('./routes/acc-controller_unpaid-fines_routes')
+const controllerSchedule = require('./routes/acc-controller_schedule_routes')
+
 app.use('/user', authRoutes);
 app.use('/user', registrationRoutes);
 app.use('/user', accountRoutes);
 app.use('/user', tripsRoutes);
-app.use('/user', finesRoutes)
+app.use('/user', finesRoutes);
+app.use('/controller', authControllerRoutes);
+app.use('/controller', controllerAccount);
+app.use('/controller', controllerPaidFines);
+app.use('/controller', controllerUnpaidFines);
+app.use('/controller', controllerSchedule);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
